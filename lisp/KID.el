9d848279509b429fc91cc836c457081aa8627f0d
KID <vanadis@yeah.net>
2016-02-11 19:53:14 +0800

    fix
    任务状态列表属性

--- lisp/KID.el
+++ lisp/KID.el
@@ -4,10 +4,12 @@
 ;;
 ;; 开始
 
+;; 全局
+
 ;; org-mode 配置
 (setq org-startup-indented t) ;; 以更明了的形式显示标题，可以用M-x org-indent-mode切换，也可以在文件头部增加#+STARTUP:indent
 (setq org-todo-keywords ;;设置TODO的任务状态列表
-      '((sequence "TODO(t!)" "STARTED(s)" "WAITTING(w)" "|" "DONE(d)" "CANCELLED(c)" "DEFERRED(f)")
+      '((sequence "TODO(t!)" "STARTED(s)" "WAITTING(w)" "|" "DONE(d !)" "CANCELLED(c @/!)" "DEFERRED(f @/!)")
         )
       )
 (setq org-todo-keyword-faces ;; 设置状态的字体颜色
